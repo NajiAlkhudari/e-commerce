651e3ffa66459fcae944da5a0e581967e82061cf
NajiAlkhudari <najikh98@gmail.com>
2024-12-24 22:45:00 +0300

    new last commit

--- src/lib/db.js
+++ src/lib/db.js
@@ -7,6 +7,10 @@ const connectDB = async () => {
     try {
         if (mongoose.connection.readyState === 0) {
             await mongoose.connect(process.env.MONGODB_URI, {
+                dbName : 'sample_mflix',
+                useNewUrlParser : true,
+                useUnifiedTopology : true,
+
             });
             console.log('MongoDB connected');
         }
