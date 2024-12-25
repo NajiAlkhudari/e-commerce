50b02298a04bd45ca83f2abb7173993dd6ddf398
NajiAlkhudari <najikh98@gmail.com>
2024-12-25 15:11:17 +0300

    new last commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        API_URL:
+          process.env.NODE_ENV === 'production'
+            ? 'https://e-commerce-ruddy-pi.vercel.app'
+            : 'http://localhost:3000',
+      },
+};
 
 export default nextConfig;
