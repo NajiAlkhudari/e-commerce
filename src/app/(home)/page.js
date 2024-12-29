b544edb58eb113bae69a92a70010c45c70b00651
NajiAlkhudari <najikh98@gmail.com>
2024-12-29 14:55:26 +0300

    refactory code 4

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -20,12 +20,12 @@ export default async function Page() {
   const products = await fetchProductsAppear();
 
   return (
-    <ul className='grid place-content-center text-center xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
+    <ul className='grid place-content-center text-center grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 '>
       {Array.isArray(products) && products.length > 0 ? (
         products.map((product) => (
           <li key={product._id}>
-            <Card className="space-y-1 flex flex-col hover:shadow-2xl px-4 py-2 m-5 w-64">
-              <Image className='h-56' src={product.image} alt={product.name} width={300} height={500} />
+            <Card className="w-40 space-y-1 flex flex-col hover:shadow-2xl px-4 py-2 m-5 md:w-64 lg:w-64 xl:w-64">
+              <Image className='h-24 md:h-56 lg:h-56 xl:h-56' src={product.image} alt={product.name} width={300} height={500} />
               <div className='flex justify-between items-center'>
                 <div>
                   <p className="font-medium text-lg">{product.name}</p>
